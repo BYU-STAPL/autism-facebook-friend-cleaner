c5014eac3557aedb7e0dce7e0e8dc353a65a97c2
Josh Bedwell <rcxwhiz@gmail.com>
2022-11-08 12:07:56 -0700

    Updated scraper code to not be crash when encountering unexpected none values

--- backend-scraper/scraper.py
+++ backend-scraper/scraper.py
@@ -24,11 +24,22 @@ def get_user_info(user):
     return info
 
 
+def get_avg(items, key):
+    if len(items) > 0:
+        return sum(filter(None, [key(item) for item in items])) / len(items)
+    else:
+        return 0
+
+
+def get_truthy_val(item, key):
+    return bool(item[key]) if key in item else False
+
+
 def get_user_stats(user, friends_info):
     try:
         profile = get_profile(user, cookies='facebook.com_cookies.txt')
     except:
-        print('Ghost account')
+        print(f'Ghost account: {user}')
         return
     posts = get_posts(
         user,
@@ -42,31 +53,31 @@ def get_user_stats(user, friends_info):
     posts = [post for post in posts]
 
     stats = {
-        'has_about': bool(profile['About']) if 'About' in profile else False,
-        'has_basic_info': bool(profile['Basic Info']) if 'Basic Info' in profile else False,
-        'has_contact_info': bool(profile['Contact info']) if 'Contact info' in profile else False,
-        'has_education': bool(profile['Education']) if 'Education' in profile else False,
-        'has_family_members': bool(profile['Family members']) if 'Family members' in profile else False,
-        'has_favorite_quotes': bool(profile['Favorite quotes']) if 'Favorite quotes' in profile else False,
+        'has_about': get_truthy_val(profile, 'About'),
+        'has_basic_info': get_truthy_val(profile, 'Basic Info'),
+        'has_contact_info': get_truthy_val(profile, 'Contact info'),
+        'has_education': get_truthy_val(profile, 'Education'),
+        'has_family_members': get_truthy_val(profile, 'Family members'),
+        'has_favorite_quotes': get_truthy_val(profile, 'Favorite quotes'),
         'follower_count': profile['Follower_count'],
         'following_count': profile['Following_count'],
         'friend_count': profile['Friend_count'],
-        'has_life_events': bool(profile['Life events']) if 'Life events' in profile else False,
-        'has_name': bool(profile['Name']),
-        'has_name_pronunciation': bool(profile['Name Pronunciation']) if 'Name Pronunciation' in profile else False,
-        'has_other_names': bool(profile['Other names']) if 'Other names' in profile else False,
-        'has_places_lived': bool(profile['Places lived']) if 'Places lived' in profile else False,
-        'has_relationship': bool(profile['Relationship']) if 'Relationship' in profile else False,
-        'has_work': bool(profile['Work']) if 'Work' in profile else False,
-        'has_cover_photo': bool(profile['cover_photo']) if 'cover_photo' in profile else False,
-        'has_cover_photo_text': bool(profile['cover_photo_text']) if 'cover_photo_text' in profile else False,
+        'has_life_events': get_truthy_val(profile, 'Life events'),
+        'has_name': get_truthy_val(profile, 'Name'),
+        'has_name_pronunciation': get_truthy_val(profile, 'Name Pronunciation'),
+        'has_other_names': get_truthy_val(profile, 'Other names'),
+        'has_places_lived': get_truthy_val(profile, 'Places lived'),
+        'has_relationship': get_truthy_val(profile, 'Relationship'),
+        'has_work': get_truthy_val(profile, 'Work'),
+        'has_cover_photo': get_truthy_val(profile, 'cover_photo'),
+        'has_cover_photo_text': get_truthy_val(profile, 'cover_photo_text'),
 
         'num_posts': len(posts),
-        'avg_num_comments': sum([post['comments'] for post in posts]) / len(posts) if len(posts) > 0 else 0,
-        'avg_num_likes': sum([post['likes'] for post in posts]) / len(posts) if len(posts) > 0 else 0,
-        'avg_num_links': sum(len(post['links']) for post in posts) / len(posts) if len(posts) > 0 else 0,
-        'avg_num_reactions': sum(post['reaction_count'] for post in posts) / len(posts) if len(posts) > 0 else 0,
-        'avg_num_shares': sum(post['shares'] for post in posts) / len(posts) if len(posts) > 0 else 0
+        'avg_num_comments': get_avg(posts, lambda x: len(x['comments'])),
+        'avg_num_likes': get_avg(posts, lambda x: x['likes']),
+        'avg_num_links': get_avg(posts, lambda x: len(x['links'])),
+        'avg_num_reactions': get_avg(posts, lambda x: x['reaction_count']),
+        'avg_num_shares': get_avg(posts, lambda x: x['shares'])
     }
 
     friends_info[user] = {
