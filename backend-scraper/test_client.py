b66a1c9eea428d2810d3b373fda5fc198940e950
Josh Bedwell <rcxwhiz@gmail.com>
2023-02-14 13:37:39 -0700

    Updated test client

--- backend-scraper/test_client.py
+++ backend-scraper/test_client.py
@@ -1,16 +1,14 @@
 import requests
 
-
-server_path = "http://127.0.0.1:5000"
-resource_endpoint = "/user_info"
-
+server_path = "https://stapl.cs.byu.edu"
+resource_endpoint = "/fb_user_info/"
 
 def test_json():
     request_body = {
-        'users': ['josh.bedwell.14', 'ameilya.monson']
+        'users': ['test', 'josh.bedwell.14', 'ameilya.monson']
     }
 
-    response = requests.get(server_path + resource_endpoint, json=request_body)
+    response = requests.post(server_path + resource_endpoint, json=request_body)
 
     if response.ok:
         print('request was successful')
